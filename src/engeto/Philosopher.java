b8abd2925364f627a8e19569054d8c5e80a772fe
Milansvob <milansvob@centrum.cz>
2021-09-10 08:02:28 +0200

    Thread.sleep(1) was added into mathod doAction in class Philosopher

--- src/engeto/Philosopher.java
+++ src/engeto/Philosopher.java
@@ -4,35 +4,43 @@ public class Philosopher implements Runnable {
     private int i;
     private Fork forkLeft;
     private Fork forkRight;
-    private int portionNumber;
+    private int portiosnNumber;
 
-    public Philosopher(int i, Fork forkLeft, Fork forkRight, int steps){
+    public Philosopher(int i, Fork forkLeft, Fork forkRight, int portionsnumber){
         this.i = i;
         this.forkLeft = forkLeft;
         this.forkRight= forkRight;
-        this.portionNumber = steps;
+        this.portiosnNumber = portionsnumber;
     }
 
-    //Method which print curent thread and his action into console
-    private void doAction(String action) {
+    //Method which print current thread and his action into console
+    //Furthermore sleep thread for 0,001s (each calling method doAction sleep thread for 0,001s)
+    //method "sleep" request exception treatment
+    private void doAction(String action) throws InterruptedException {
         System.out.println(Thread.currentThread().getName() + " " + action);
+        Thread.sleep(1);
     }
 
     @Override
     public void run() {
-        //Cycle for eating demanded portions
-            for (int i = 0; i < portionNumber; i++) {
+
+        try {
+            //Cycle for eating demanded portions
+            for (int i = 0; i < portiosnNumber; i++) {
                 doAction(": Thinking");
-                //It ensure that only one thread will be used during left fork acquiring
+//It ensure that certain fork is picked by philosopher and no-one else can pick up the same for until to be put down
                 synchronized (forkLeft) {
                     doAction(": Picked up left fork");
-                    //It ensure that only one thread will be used during right fork acquiring
+//It ensure that certain fork is picked by philosopher and no-one else can pick up the same for until to be put  down
                     synchronized (forkRight) {
-                        doAction(": Picked up right fork - eating");
+                        doAction(": Picked up right fork - eating dinner number " + (i + 1));
                         doAction(": Put down right fork");
                     }
                     doAction(": Put down left fork. Returning to thinking");
                 }
             }
+        } catch (InterruptedException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }
